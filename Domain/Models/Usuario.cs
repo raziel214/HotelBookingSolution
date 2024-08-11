38c4326f7144216dd230431347c905e7a7e14a4e
raziel214 <soulreavers214@gmail.com>
2024-08-11 16:48:36 -0500

    creación dbcontex
    - se crea el dbcontext de la base de datos
    -ajuste git ignore

---
+++ Domain/Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Documento { get; set; }
+        public string TipoDocumento { get; set; }
+        public int IdRol { get; set; }
+
+        // Relación con Rol
+        public Rol Rol { get; set; }
+
+    }
+}
--- obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc7dedc3447b373e934cc10a2e8167dbea264b37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+968aeea37ed39b4671edb31139ec6ad6c63573cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
